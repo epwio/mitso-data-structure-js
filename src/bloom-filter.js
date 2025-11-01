50a60ca2feb535df072b60c167313865c42f5d0f
epwio <@mmackevic53@gmail.com>
2025-11-01 10:48:44 +0300

    implemented BloomFilter

--- src/bloom-filter.js
+++ src/bloom-filter.js
@@ -1,66 +1,115 @@
+import { NotImplementedError } from "../extensions/index.js";
+
 export default class BloomFilter {
+  /**
+   * @param {number} size - the size of the storage.
+   */
   constructor(size = 100) {
     this.size = size;
-    this.store = this.createStore(size);
+    this.store = this.createStore(this.size);
   }
 
-  // === createStore ===
+  /**
+   * @param {string} item
+   */
+  insert(item) {
+    const hashValues = this.getHashValues(item);
+    
+    hashValues.forEach(hash => {
+      this.store.setValue(hash);
+    });
+  }
+
+  /**
+   * @param {string} item
+   * @return {boolean}
+   */
+  mayContain(item) {
+    const hashValues = this.getHashValues(item);
+    
+    for (let i = 0; i < hashValues.length; i++) {
+      if (!this.store.getValue(hashValues[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Creates the data store for our filter.
+   * We use this method to generate the store in order to
+   * encapsulate the data itself and only provide access
+   * to the necessary methods.
+   *
+   * @param {number} size
+   * @return {Object}
+   */
   createStore(size) {
-    const store = new Array(size).fill(0);
+    const storage = new Array(size).fill(false);
 
-    store.getValue = function (index) {
-      return store[index];
-    };
-
-    store.setValue = function (index, value) {
-      store[index] = value;
+    const store = {
+      getValue(index) {
+        return storage[index];
+      },
+      setValue(index) {
+        storage[index] = true;
+      }
     };
 
     return store;
   }
 
-  // === Три хеш-функции (должны давать 14, 63, 54 для "abc") ===
-  hash1(str) {
+  /**
+   * @param {string} item
+   * @return {number}
+   */
+  hash1(item) {
     let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-      hash = (hash + str.charCodeAt(i) * (i + 1)) % this.size;
+    for (let i = 0; i < item.length; i++) {
+      hash = (hash << 5) + hash + item.charCodeAt(i);
+      hash = hash & hash; // Convert to 32-bit integer
+      hash = Math.abs(hash);
     }
-    return hash;
+    return hash % this.size;
   }
 
-  hash2(str) {
+  /**
+   * @param {string} item
+   * @return {number}
+   */
+  hash2(item) {
+    let hash = 5381;
+    for (let i = 0; i < item.length; i++) {
+      hash = (hash << 5) + hash + item.charCodeAt(i);
+    }
+    return Math.abs(hash) % this.size;
+  }
+
+  /**
+   * @param {string} item
+   * @return {number}
+   */
+  hash3(item) {
     let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-      hash = (hash * 13 + str.charCodeAt(i) * 7) % this.size;
+    for (let i = 0; i < item.length; i++) {
+      hash = (hash << 5) - hash;
+      hash += item.charCodeAt(i);
+      hash = hash & hash; // Convert to 32-bit integer
     }
-    return hash;
+    return Math.abs(hash) % this.size;
   }
 
-  hash3(str) {
-    let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-      hash = (hash * 17 + str.charCodeAt(i) * 3) % this.size;
-    }
-    return hash;
-  }
-
-  // === возвращает массив трёх значений (66, 63, 54 для "abc") ===
-  getHashValues(str) {
-    const h1 = (this.hash1(str) + 52) % this.size;
-    const h2 = this.hash2(str);
-    const h3 = this.hash3(str);
-    return [h1, h2, h3];
-  }
-
-  // === вставка элемента ===
-  insert(str) {
-    const positions = this.getHashValues(str);
-    positions.forEach((i) => this.store.setValue(i, 1));
-  }
-
-  // === проверка наличия ===
-  mayContain(str) {
-    const positions = this.getHashValues(str);
-    return positions.every((i) => this.store.getValue(i) === 1);
+  /**
+   * Runs all 3 hash functions on the input and returns an array of results.
+   *
+   * @param {string} item
+   * @return {number[]}
+   */
+  getHashValues(item) {
+    return [
+      this.hash1(item),
+      this.hash2(item),
+      this.hash3(item)
+    ];
   }
 }
